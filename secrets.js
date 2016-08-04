280df4e74b7e8bc17cd57f2c1227b4450c8387f5
Koray Gocmen <gocmen.koray@gmail.com>
2016-08-04 04:03:23 -0400

    commented out development env

--- secrets.js
+++ secrets.js
@@ -3,7 +3,7 @@ global.jwtTokenSecret = 'd36jFLn235gs57dgldfNAK_JNDSG2mdfl324smJ23589fmY';
 global.localConnectionString = 'postgres://localhost:5432/jukebox_messaging_api';
 global.productionConnectionString = 'postgres://gmdhdmxywprfcs:r5ze6W2-squL7QET43mAebVftU@ec2-54-243-204-221.compute-1.amazonaws.com:5432/dm8r768ur90kj';
 
-process.env.NODE_ENV = 'development';
+//process.env.NODE_ENV = 'development';
 //process.env.NODE_ENV = 'production';
 
 global.nameDatabase_development = 'jukebox_messaging_api';
