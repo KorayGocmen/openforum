2659ead983d33b59f8091e2b6a138bd7dfa6c44b
Koray Gocmen <gocmen.koray@gmail.com>
2016-08-04 03:44:15 -0400

    add secrets and favicon

--- secrets.js
+++ secrets.js
@@ -1,7 +1,7 @@
 // Global jwt token
 global.jwtTokenSecret = 'd36jFLn235gs57dgldfNAK_JNDSG2mdfl324smJ23589fmY';
 global.localConnectionString = 'postgres://localhost:5432/jukebox_messaging_api';
-global.productionConnectionString = '';
+global.productionConnectionString = 'postgres://gmdhdmxywprfcs:r5ze6W2-squL7QET43mAebVftU@ec2-54-243-204-221.compute-1.amazonaws.com:5432/dm8r768ur90kj';
 
 process.env.NODE_ENV = 'development';
 //process.env.NODE_ENV = 'production';
@@ -10,6 +10,6 @@ global.nameDatabase_development = 'jukebox_messaging_api';
 global.usernameDatabase_development = 'koraygocmen';
 global.passwordDatabase_development = 'tk95961523';
 
-global.nameDatabase_production = '';
-global.usernameDatabase_production = '';
-global.passwordDatabase_production = '';
+global.nameDatabase_production = 'dm8r768ur90kj';
+global.usernameDatabase_production = 'gmdhdmxywprfcs';
+global.passwordDatabase_production = 'r5ze6W2-squL7QET43mAebVftU';
