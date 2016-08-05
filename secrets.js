f71718f493b15479c0345e33e7fa4bc28b41f7ba
Koray Gocmen <gocmen.koray@gmail.com>
2016-08-05 19:05:32 -0400

    development key word was fixed

--- secrets.js
+++ secrets.js
@@ -3,7 +3,7 @@ global.jwtTokenSecret = 'd3ashd82YwrYYasHfba_34jasdHFJjfnsdf23';
 global.localConnectionString = 'postgres://localhost:5432/openforum';
 global.productionConnectionString = 'postgres://erlecfufeoxbbr:EbX8KIu0sX2PCSRLaG0XEiwB6g@ec2-23-21-234-201.compute-1.amazonaws.com:5432/dfahtleu0miiic';
 
-process.env.NODE_ENV = 'development';
+//process.env.NODE_ENV = 'development';
 //process.env.NODE_ENV = 'production';
 
 global.nameDatabase_development = 'openforum';
