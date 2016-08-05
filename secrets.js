68467fb2e44125ef2493cbe0a7d91934d0df36e4
Koray Gocmen <gocmen.koray@gmail.com>
2016-08-05 18:50:09 -0400

    Database Secrets

--- secrets.js
+++ secrets.js
@@ -1,7 +1,7 @@
 // Global jwt token
 global.jwtTokenSecret = 'd3ashd82YwrYYasHfba_34jasdHFJjfnsdf23';
 global.localConnectionString = 'postgres://localhost:5432/openforum';
-global.productionConnectionString = '';
+global.productionConnectionString = 'postgres://erlecfufeoxbbr:EbX8KIu0sX2PCSRLaG0XEiwB6g@ec2-23-21-234-201.compute-1.amazonaws.com:5432/dfahtleu0miiic';
 
 process.env.NODE_ENV = 'development';
 //process.env.NODE_ENV = 'production';
@@ -10,6 +10,6 @@ global.nameDatabase_development = 'openforum';
 global.usernameDatabase_development = 'koraygocmen';
 global.passwordDatabase_development = 'tk95961523';
 
-global.nameDatabase_production = '';
-global.usernameDatabase_production = '';
-global.passwordDatabase_production = '';
+global.nameDatabase_production = 'dfahtleu0miiic';
+global.usernameDatabase_production = 'erlecfufeoxbbr';
+global.passwordDatabase_production = 'EbX8KIu0sX2PCSRLaG0XEiwB6g';
